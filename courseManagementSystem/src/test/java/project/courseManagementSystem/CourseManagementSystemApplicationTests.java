c056ef73025984cf6e58ae43f260e3b0d2993765
zeynepsl <zeyneprsalman@gmail.com>
2021-10-11 17:10:52 +0300

    layers created and entities added.

---
+++ courseManagementSystem/src/test/java/project/courseManagementSystem/CourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project.courseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
