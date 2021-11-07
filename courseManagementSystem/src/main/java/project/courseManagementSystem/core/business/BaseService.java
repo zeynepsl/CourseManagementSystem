8a071ae37e5dcf66f6b3fd1e473863e51c2613fd
zeynepsl <zeyneprsalman@gmail.com>
2021-11-07 17:03:42 +0300

    parameters refactored

--- courseManagementSystem/src/main/java/project/courseManagementSystem/core/business/BaseService.java
+++ courseManagementSystem/src/main/java/project/courseManagementSystem/core/business/BaseService.java
@@ -7,7 +7,7 @@ import project.courseManagementSystem.core.utilities.results.Result;
 
 public interface BaseService<T> {
 	public Result add(T entity);
-	public Result delete(T entity);
+	public Result delete(int id);
 	public Result update(T entity);
 	public DataResult<T> getById(int id);
 	public DataResult<List<T>> getAll();
