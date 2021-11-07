b663ffc202771dbd670f328b08baf195071df75a
zeynepsl <zeyneprsalman@gmail.com>
2021-11-07 14:26:06 +0300

    updated

--- courseManagementSystem/src/main/java/project/courseManagementSystem/business/abstracts/StudentService.java
+++ courseManagementSystem/src/main/java/project/courseManagementSystem/business/abstracts/StudentService.java
@@ -1,7 +1,11 @@
 package project.courseManagementSystem.business.abstracts;
 
 import project.courseManagementSystem.core.business.BaseService;
+import project.courseManagementSystem.core.utilities.results.DataResult;
+import project.courseManagementSystem.core.utilities.results.Result;
 import project.courseManagementSystem.entities.concretes.Student;
 
 public interface StudentService extends BaseService<Student>{
+	public Result register(Student student);
+	public DataResult<Student> findByEmail(String email);
 }
--- courseManagementSystem/src/main/java/project/courseManagementSystem/business/abstracts/UserService.java
+++ courseManagementSystem/src/main/java/project/courseManagementSystem/business/abstracts/UserService.java
@@ -1,9 +1,11 @@
 package project.courseManagementSystem.business.abstracts;
 
 import project.courseManagementSystem.core.business.BaseService;
+import project.courseManagementSystem.core.utilities.results.DataResult;
 import project.courseManagementSystem.core.utilities.results.Result;
 import project.courseManagementSystem.entities.concretes.User;
 
 public interface UserService extends BaseService<User>{
 	public Result existsByEmail(String email);
+	public DataResult<User> findByEmail(String email);
 }
