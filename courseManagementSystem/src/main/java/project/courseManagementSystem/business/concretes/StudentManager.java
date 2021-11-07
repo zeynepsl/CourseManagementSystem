e300a40fc6f5d7aef8b81b06162d5ef7a1c2940f
zeynepsl <zeyneprsalman@gmail.com>
2021-11-07 20:21:48 +0300

    refactor

--- courseManagementSystem/src/main/java/project/courseManagementSystem/business/concretes/StudentManager.java
+++ courseManagementSystem/src/main/java/project/courseManagementSystem/business/concretes/StudentManager.java
@@ -1,4 +1,4 @@
- package project.courseManagementSystem.business.concretes;
+package project.courseManagementSystem.business.concretes;
 
 import java.util.List;
 
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import project.courseManagementSystem.business.abstracts.StudentService;
 import project.courseManagementSystem.business.abstracts.UserService;
 import project.courseManagementSystem.business.validationRules.StudentValidatorService;
-import project.courseManagementSystem.core.adapters.mernis.UserCheckService;
 import project.courseManagementSystem.core.email.EmailCheckService;
 import project.courseManagementSystem.core.utilities.results.DataResult;
 import project.courseManagementSystem.core.utilities.results.ErrorDataResult;
@@ -20,69 +19,63 @@ import project.courseManagementSystem.dataAccess.abstracts.StudentDao;
 import project.courseManagementSystem.entities.concretes.Student;
 
 @Service
-public class StudentManager implements StudentService{
+public class StudentManager implements StudentService {
 
 	private StudentDao studentDao;
 	private StudentValidatorService studentValidatorService;
-	private UserCheckService userCheckService;
 	private EmailCheckService emailCheckService;
 	private UserService userService;
-	
+
 	@Autowired
 	public StudentManager(StudentDao studentDao, StudentValidatorService studentValidatorService,
-			UserCheckService userCheckService, EmailCheckService emailCheckService, UserService userService) {
+			EmailCheckService emailCheckService, UserService userService) {
 		super();
 		this.studentDao = studentDao;
 		this.studentValidatorService = studentValidatorService;
-		this.userCheckService = userCheckService;
 		this.emailCheckService = emailCheckService;
 		this.userService = userService;
 	}
 
-	
-	
 	public Result register(Student student) {
-		
-		if(!emailCheckService.emailCheck(student.getEmail())) {
-			return new ErrorResult("invalid email");
-		}
-		
-		if(!studentValidatorService.checkIfStudentInfoIsFull(student)) {
-			return new ErrorResult("enter all your information completely");		
-		}
-		
-		//refactor:
-		if( (findByEmail( student.getEmail()) ).getData() != null) {
+
+		// refactor: (findByEmail( student.getEmail()) ).getData() != null
+		//returns true if saved to database:
+		if (userService.existsByEmail(student.getEmail())) {
 			return new ErrorResult("this student is already exist");
 		}
+
+		if (!studentValidatorService.checkIfStudentInfoIsFull(student)) {
+			return new ErrorResult("enter all your information completely");
+		}
 		
+		if (!emailCheckService.emailCheck(student.getEmail())) {
+			return new ErrorResult("invalid email");
+		}
+
 		add(student);
 		return new SuccessResult("Successfully registered");
-		
+
 	}
-	
-	
+
 	@Override
 	public Result add(Student entity) {
 		studentDao.save(entity);
 		return new SuccessResult("Student added!");
 	}
-	
 
 	@Override
 	public Result delete(int id) {
-		if(getById(id).getData() == null) {
+		if (getById(id).getData() == null) {
 			return new ErrorResult("Student is not exist!");
 		}
 		studentDao.deleteById(id);
 		return new SuccessResult("Student deleted!");
 	}
-	
-	
+
 	@Override
 	public Result update(Student entity) {
 		Student updatedStudent = getById(entity.getId()).getData();
-		
+
 		updatedStudent.setEmail(entity.getEmail());
 		updatedStudent.setBirthDate(entity.getBirthDate());
 		updatedStudent.setFirstName(entity.getFirstName());
@@ -92,7 +85,7 @@ public class StudentManager implements StudentService{
 		updatedStudent.setPassword(entity.getPassword());
 		updatedStudent.setPhoneNumber(entity.getPhoneNumber());
 		updatedStudent.setSchoolName(entity.getSchoolName());
-		
+
 		studentDao.save(updatedStudent);
 		return new SuccessResult("Student updated!");
 	}
@@ -100,7 +93,7 @@ public class StudentManager implements StudentService{
 	@Override
 	public DataResult<Student> getById(int id) {
 		Student student = studentDao.findById(id);
-		if(student == null) {
+		if (student == null) {
 			return new ErrorDataResult<Student>(null, "Student is not exist!");
 		}
 		return new SuccessDataResult<Student>(student, "Student viewed!");
@@ -111,26 +104,14 @@ public class StudentManager implements StudentService{
 		return new SuccessDataResult<List<Student>>(studentDao.findAll(), "Students listed");
 	}
 
-	
 	@Override
-	public DataResult<Student> findByEmail(String email) {
-		for(Student student : studentDao.findAll()) {
-			if( (student.getEmail()).equals(email)) {
+	public DataResult<Student> getByEmail(String email) {
+		for (Student student : studentDao.findAll()) {
+			if ((student.getEmail()).equals(email)) {
 				return new SuccessDataResult<Student>(student, "student listed");
 			}
 		}
 		return new ErrorDataResult<Student>(null, "student is not exist");
 	}
-	
-	private boolean checkIfStudentExist(int id) {
-		Student student = studentDao.findById(id);
-		if(student == null) {
-			return false;
-		}
-		return true;
-	}
-
-
-	
 
 }
